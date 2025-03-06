42ac95f7c7b61a1c64788b879f2be716cc71e15a
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-03-06 11:58:23 +0300

    App update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,6 @@
 //import { useState } from "react";//
 
 import Profile from "../Profile/Profile";
-import userData from "../userData.json";
-import FriendList from "../FriendList/FriendList";
-import friends from "../friends.json";
-import TransactionHistory from "../TransactionHistory/TransactionHistory";
-import transactions from "../transactions.json";
-import Work from "../Work/Work";
 
 export default function App() {
   return (
@@ -20,13 +14,6 @@ export default function App() {
         views={userData.stats.views}
         likes={userData.stats.likes}
       />
-      <>
-        <p>Selected language: </p>
-        <Work />
-      </>
-
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
     </>
   );
 }

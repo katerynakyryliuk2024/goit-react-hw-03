301536d268eaff95d53df2effd6934d07a97b59e
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-03-06 13:05:30 +0300

    formik create

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,24 @@
 import css from "./ContactForm.module.css";
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 export default function ContactForm() {
   return (
     <Formik initialValues={{}} onSubmit={() => {}}>
-      ...
+      <Form className={css.form}>
+        <div className={css.group}>
+          <label className={css.label}>Name</label>
+          <Field className={css.input} type="text" name="name" />
+          <ErrorMessage className={css.error} name="name" component="span" />
+        </div>
+        <div className={css.group}>
+          <label className={css.label}>Number</label>
+          <Field className={css.input} type="tel" name="number" />
+          <ErrorMessage className={css.error} name="number" component="span" />
+        </div>
+        <button className={css.button} type="submit">
+          Add contact
+        </button>
+      </Form>
     </Formik>
   );
 }
